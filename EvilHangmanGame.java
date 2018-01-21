389e894df8d812a1a92efe063ccb75be8065aea5
Matthew Robertson <mattrobertson1414@gmail.com>
2018-01-21 12:46:59 -0700

    implemented interface and created Main.

---
+++ EvilHangmanGame.java
@@ -0,0 +1,18 @@
+package hangman;
+
+import java.io.File;
+import java.util.*;
+
+public class EvilHangmanGame implements IEvilHangmanGame {
+  //@SuppressWarnings("serial")
+	//public static class GuessAlreadyMadeException extends Exception { }
+
+	public void startGame(File dictionary, int wordLength){ }
+
+	public Set<String> makeGuess(char guess) throws IEvilHangmanGame.GuessAlreadyMadeException{
+
+
+
+    return null;
+  }
+}
