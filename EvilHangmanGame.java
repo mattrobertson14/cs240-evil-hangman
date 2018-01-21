fa03f9f47e9934d3f6f82cff75b0e4a5905bd69a
Matthew Robertson <mattrobertson1414@gmail.com>
2018-01-21 13:27:45 -0700

    reads the file in and starts the game

--- EvilHangmanGame.java
+++ EvilHangmanGame.java
@@ -1,18 +1,32 @@
 package hangman;
 
-import java.io.File;
+import java.io.*;
 import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
   //@SuppressWarnings("serial")
 	//public static class GuessAlreadyMadeException extends Exception { }
 
-	public void startGame(File dictionary, int wordLength){ }
+  private TreeSet<String> set;
+
+	public void startGame(File dictionary, int wordLength){
+    try{
+      Scanner scanner = new Scanner(dictionary);
+      set = new TreeSet<String>();
+      while (scanner.hasNext()){
+        set.add(scanner.next());
+      }
+
+      scanner.close();
+    } catch (IOException ex){
+      System.out.println("Error: File not found");
+    }
+  }
 
 	public Set<String> makeGuess(char guess) throws IEvilHangmanGame.GuessAlreadyMadeException{
 
 
 
-    return null;
+    return set;
   }
 }
--- Main.java
+++ Main.java
@@ -6,7 +6,36 @@ import java.util.*;
 public class Main {
 
   public static void main(String[] args){
-    System.out.println("Comming Soon... EVIL HANGMAN");
+
+    if (args.length < 1){
+      System.out.println("\n\tERROR: Please enter a File, word length, and number of guesses\n");
+      System.exit(0);
+    }
+    String dictionaryFile = args[0];
+
+    if (args.length < 2){
+      System.out.println("\n\tERROR: Please enter a word length and a number of guesses\n");
+      System.exit(0);
+    }
+    int wordLength = Integer.parseInt(args[1]);
+
+    if (args.length < 3){
+      System.out.println("\n\tERROR: Please enter a number of guesses\n");
+      System.exit(0);
+    }
+    int maxGuesses = Integer.parseInt(args[2]);
+    File file = new File(dictionaryFile);
+
+    EvilHangmanGame game = new EvilHangmanGame();
+
+    int guesses = 0;
+    game.startGame(file, wordLength);
+
+    while (guesses < maxGuesses){
+      System.out.println("guess made");
+      guesses++;
+    }
+
   }
 
 }
